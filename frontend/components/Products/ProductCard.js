1396fdc056859aab1926bbc01e4274836cea0408
Del <124511388+Delvinah@users.noreply.github.com>
2023-03-01 13:35:58 +0300

    modified

--- frontend/components/Products/ProductCard.js
+++ frontend/components/Products/ProductCard.js
@@ -1,23 +1,22 @@
 import React from 'react'
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import CardContent from '@mui/material/CardContent';
-import Skeleton from '@mui/material/Skeleton';
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
+import Skeleton from '@material-ui/lab/Skeleton';
+import Chip from '@material-ui/core/Chip';
+import Stack from '@material-ui/core/Stack';
 
-function ProductCard() {
-    const loading = false;
-  return (
-    <Card sx={{ maxWidth: 345 }} className="mb-5 p-2">
+function ProductCard({loading,data}) {
+    return (
+    <Card style={{ maxWidth: 345 }} className="mb-5 p-2">
         {loading ? (
             <>
             <CardMedia>
                 <Skeleton animation="wave" variant="rectangular" height={194} />
             </CardMedia>
             <div className='p-2'>
-                <Typography variant="h1" color="text.secondary">
+                <Typography variant="h6" color="textSecondary">
                     <Skeleton animation="wave" variant="text" />
                 </Typography>
             </div>
@@ -26,18 +25,18 @@ function ProductCard() {
               <>
                 <CardMedia
                     component="img"
-                    height="194"
-                    image="https://res.cloudinary.com/murste/image/upload/v1619359682/ecom_xrzgmm.jpg"
+                    height={194}
+                    image={data.productImage}
                     alt="Paella dish"/>
                 <CardContent>
                     <div className='p-1'>
                         <div className='float-left'>
-                            <h6>Product Name</h6>
+                            <h6>{data.productName}</h6>
                         </div>
                         <div className='float-right'>
                         <Stack direction="row" spacing={1}>
                             {/* <Chip label="primary" color="primary" /> */}
-                            <Chip label="free" color="success" size="small" />
+                            <Chip label={data.productType} color="success" size="small" />
                         </Stack>
                         </div>
                     </div>
