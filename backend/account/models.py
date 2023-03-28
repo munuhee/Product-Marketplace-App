9a61521550d19228413bdc009479f1d964d5f325
Munuhee <51537638+munuhee@users.noreply.github.com>
2023-03-28 14:04:27 +0300

    add a balance field

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    balance = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+class CustomUser(AbstractUser):
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
-    def __str__(self):
-        return f'{self.user.username} Profile'
 
 
 class Vendor(models.Model):
